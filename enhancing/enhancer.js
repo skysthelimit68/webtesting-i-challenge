38aeaec8fabca4d3d15cc74c67b1f7119a4473c2
Ming Liu <mliu@macpro.home>
2019-06-17 16:41:09 -0400

    create methods for succeed, fail, repair, get and preventOutOfBound, create test for all methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,20 +3,43 @@ module.exports = {
   fail,
   repair,
   get,
+  preventOutOfBound
 };
 
 function succeed(item) {
-  return { ...item };
+  const max = 20
+  const newItem = preventOutOfBound(item)
+  const newEnhance = newItem.enhancement >= max ? max : newItem.enhancement + 1
+  return {...newItem, enhancement : newEnhance}
 }
 
+// If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
 function fail(item) {
-  return { ...item };
+  const min = 0;
+  const newItem = preventOutOfBound(item);
+  const newEnhance =  newItem.enhancement > 16 ? (newItem.enhancement-1) : newItem.enhancement;
+  const newDura = newItem.enhancement < 15 ? (newItem.durability-5) : (newItem.durabiilty-10);
+  
+  return { ...newItem, enhancement : newEnhance, durability : Math.max(newDura, min)};
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = preventOutOfBound(item);
+  return { ...newItem, durability : 100  };
 }
 
 function get(item) {
-  return { ...item };
+  const newItem = preventOutOfBound(item)
+  const newName = newItem.enhancement > 0 ? `[+${newItem.enhancement}] ${newItem.name}` : newItem.name;
+  console.log(newName);
+  return { ...newItem, name : newName };
+}
+
+function preventOutOfBound(item) {
+  item.enhancement = item.enhancement < 0 ? 0 : item.enhancement > 20 ? 20 : item.enhancement
+  item.durability = item.durability < 0 ? 0 : item.durability > 100 ? 100 : item.durability
+  return item;
 }
---
+++ enhancing/enhancer.test.js
@@ -0,0 +1,58 @@
+const {succeed, fail, repair, get, preventOutOfBound} = require('./enhancer.js');
+// test away!
+
+describe('enhancer.js', () => {
+    
+    
+    describe('repair test', () => {
+        it('restores durabiity to 100', ()=> {
+            expect(repair({ durability: 89}).durability).toBe(100);
+            expect(repair({ durability: -89}).durability).toBe(100);
+            expect(repair({ durability: 100}).durability).toBe(100);
+            expect(repair({ durability: 0})).toEqual({durability : 100});
+
+        })
+    })
+    it.todo('durability should have a max value of 100')
+
+    describe('succeed test', () => {
+        it('increment enhancement to up to 20', () => {
+            expect(succeed({enhancement : 20}).enhancement).toBe(20);
+            expect(succeed({enhancement : 12}).enhancement).toBe(13);
+            expect(succeed({enhancement : -3}).enhancement).toBe(1);
+            expect(succeed({enhancement : 128}).enhancement).toBe(20);
+        })
+    })
+    it.todo('enhancement should have max value of 20')
+
+    describe('fail test', () => {
+        it('decrement enhancement down to 16 and durability down to 0', () => {
+            expect(fail({enhancement: 25, durability: 186}).enhancement).toBe(19) && expect(fail({enhancement: 25, durability: 186}).durability).toBe(90);
+            expect(fail({enhancement: -25, durability: -186}).enhancement).toBe(0) && expect(fail({enhancement: -25, durability: -186}).durability).toBe(0);
+            expect(fail({enhancement: 17, durability: 12}).enhancement).toBe(16) && expect(fail({enhancement: 20, durability: 12}).durability).toBe(2);
+            expect(fail({enhancement: 10, durability: 12}).enhancement).toBe(10) && expect(fail({enhancement: 10, durability: 12}).durability).toBe(7);
+            expect(fail({enhancement: 15, durability: 7}).enhancement).toBe(15) && expect(fail({enhancement: 15, durability: 7}).durability).toBe(0);
+
+        })
+    })
+
+    describe('out of bound test', () => {
+        it('return floor/ceiling/original value based on if the value is out of bound', () => {
+            expect(preventOutOfBound({enhancement: 25, durability: 186}).enhancement).toBe(20) && expect(preventOutOfBound({enhancement: 25, durability: 186}).durability).toBe(100);
+            expect(preventOutOfBound({enhancement: -25, durability: -186}).enhancement).toBe(0) && expect(preventOutOfBound({enhancement: 25, durability: 186}).durability).toBe(0);
+            expect(preventOutOfBound({enhancement: 16, durability: 33}).enhancement).toBe(16) && expect(preventOutOfBound({enhancement: 16, durability: 33}).durability).toBe(33);
+
+        })
+    })
+
+    describe('get test', () => {
+        it('return name with enhancement if enhancement is greater than 0', () => {
+            expect(get({enhancement : -3, name}).name).toBe({name}.name)
+            expect(get({enhancement : 12, name}).name).toBe(`[+12] ${{name}.name}`)
+            expect(get({enhancement : 29, name}).name).toBe(`[+20] ${{name}.name}`)
+
+
+        })
+    })
+
+})
